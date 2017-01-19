3498856c7da6248860d7c928dd7e4c4165059325
pootle73 <neil_rapley@hotmail.com>
2017-01-19 16:51:51 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -251,7 +251,7 @@ ibc.load(options, function (err, cc){														//parse/load chaincode, respo
 
 		// ---- To Deploy or Not to Deploy ---- //
 		if (!cc.details.deployed_name || cc.details.deployed_name === '') {					//yes, go deploy
-            console.log('I'm going to timeout\n');
+            console.log('I am going to timeout\n');
 			cc.deploy('init', ['99'], {delay_ms: 600000}, function(e){ 						//delay_ms is milliseconds to wait after deploy for conatiner to start, 50sec recommended
 				check_if_deployed(e, 1);
 			});
